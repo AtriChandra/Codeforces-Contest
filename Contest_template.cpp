bfd38bd1be9c576c52b29c2f2cdc23d630dee76f
Atri Chandra <121769804+AtriChandra@users.noreply.github.com>
2024-02-27 21:48:04 +0530

    Create Contest_template.cpp
    This is the template that I follow while giving contest on Codeforces

---
+++ Contest_template.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define fs(lcv,hi) for(long long lcv=0;lcv<hi; lcv++)
+#define fb(lcv,hi) for(long long lcv=hi;lcv>=0; lcv--)
+#define co cout <<
+#define ci cin >>
+#define vv vector<long long>
+#define nl <<"\n"
+
+void solve() {
+    
+}
+
+int main() {
+    ll t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}

eed9750c8c7e2f959ef93b2e462806b4a08d75bd
Atri Chandra <121769804+AtriChandra@users.noreply.github.com>
2024-02-29 20:45:06 +0530

    Create A. Shuffle Party.cpp

---
+++ A. Shuffle Party.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define fs(lcv,hi) for(long long lcv=0;lcv<hi; lcv++)
+#define fb(lcv,hi) for(long long lcv=hi;lcv>=0; lcv--)
+#define co cout <<
+#define ci cin >>
+#define vv vector<long long>
+#define nl <<"\n"
+
+void solve() {
+    ll n;
+    ci n;
+    ll t=log2(n);
+    ll ans=pow(2,t);
+    co ans nl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}

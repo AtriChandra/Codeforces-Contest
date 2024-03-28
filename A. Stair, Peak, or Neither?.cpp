787ae556e9ab13bdee94cf7d7042100a0ce87979
Atri Chandra <121769804+AtriChandra@users.noreply.github.com>
2024-03-28 20:54:04 +0530

    Create A. Stair, Peak, or Neither?.cpp

---
+++ A. Stair, Peak, or Neither?.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb emplace_back
+#define fs(i,x,y) for(int i=x;i<y;i++)
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define mii map<int,int>
+#define pii pair<int,int>
+#define vvi vector<vector<int>>
+#define vi vector<int>
+#define vpi vector<pair<int,int>>
+#define nl "\n"
+
+
+vector<vector<int>> TLE(40000, vector<int>(7));
+
+void solve() {
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a<b && b<c) 
+    	cout<<"STAIR"<<nl;
+    else if(a<b && b>c)
+    	cout<<"PEAK"<<nl;
+    else cout<<"NONE"<<nl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
